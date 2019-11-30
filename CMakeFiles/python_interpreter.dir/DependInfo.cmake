ca14da3ef9e1f6b0194e4851705318191d2ceba7
Amagi_Yukisaki <xingzeyu2001@163.com>
2019-11-30 13:57:57 +0800

    Upload Files

---
+++ CMakeFiles/python_interpreter.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/xzy/Desktop/CppPython/generated/Python3BaseListener.cpp" "/Users/xzy/Desktop/CppPython/CMakeFiles/python_interpreter.dir/generated/Python3BaseListener.cpp.o"
+  "/Users/xzy/Desktop/CppPython/generated/Python3Lexer.cpp" "/Users/xzy/Desktop/CppPython/CMakeFiles/python_interpreter.dir/generated/Python3Lexer.cpp.o"
+  "/Users/xzy/Desktop/CppPython/generated/Python3Listener.cpp" "/Users/xzy/Desktop/CppPython/CMakeFiles/python_interpreter.dir/generated/Python3Listener.cpp.o"
+  "/Users/xzy/Desktop/CppPython/generated/Python3Parser.cpp" "/Users/xzy/Desktop/CppPython/CMakeFiles/python_interpreter.dir/generated/Python3Parser.cpp.o"
+  "/Users/xzy/Desktop/CppPython/src/main.cpp" "/Users/xzy/Desktop/CppPython/CMakeFiles/python_interpreter.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "generated"
+  "third_party"
+  "third_party/runtime/src"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/xzy/Desktop/CppPython/CMakeFiles/antlr4-cpp-runtime.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- README.md
+++ README.md
@@ -1,36 +1,9 @@
-# SJTU2019_HW2
+# Sjtu2019_Python3_Interpreter
 
-#### Description
-{**When you're done, you can delete the content in this README and update the file with details for others getting started with your repository**}
+Homework of Python3 Interpreter
 
-#### Software Architecture
-Software architecture description
+上海交通大学2019级第一学期程序设计 Python3 解释器 大作业
 
-#### Installation
+Please don't copy my code, since it's still under heavy development.
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-#### Instructions
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-#### Contribution
-
-1.  Fork the repository
-2.  Create Feat_xxx branch
-3.  Commit your code
-4.  Create Pull Request
-
-
-#### Gitee Feature
-
-1.  You can use Readme\_XXX.md to support different languages, such as Readme\_en.md, Readme\_zh.md
-2.  Gitee blog [blog.gitee.com](https://blog.gitee.com)
-3.  Explore open source project [https://gitee.com/explore](https://gitee.com/explore)
-4.  The most valuable open source project [GVP](https://gitee.com/gvp)
-5.  The manual of Gitee [https://gitee.com/help](https://gitee.com/help)
-6.  The most popular members  [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+请不要抄袭我的代码，它们仍在被开发。（退学警告）
